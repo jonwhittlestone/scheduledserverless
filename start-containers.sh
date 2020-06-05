5f479b5ef969aa554b5db404758cfc3482fb42d8
Jon Whittlestone <dev@howapped.com>
2020-06-05 07:36:36 +0100

    If repo is already cloned, just update

--- start-containers.sh
+++ start-containers.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-# clone repo, start containers
-rm -rf /root/scheduled-serverless-startup
-mkdir /root/scheduled-serverless-startup
-git clone https://github.com/jonwhittlestone/scheduled-serverless-startup.git /root/scheduled-serverless-startup
+
+if [ -d "/root/scheduled-serverless-startup" ] 
+then
+    git pull origin master
+else
+    cd /root/scheduled-serverless-startup
+     git clone https://github.com/jonwhittlestone/scheduled-serverless-startup.git /root/scheduled-serverless-startup
+fi
 cd /root/scheduled-serverless-startup/app
 docker-compose up -d
